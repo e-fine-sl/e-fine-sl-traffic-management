8f5cbacb4461d5bdf7aeaed1479ef407b0e518f0
kavishka-rasanjana <krasanjana81@gmail.com>
2026-04-23 13:06:36 +0530

    change render url with local ip

--- mobile_app/lib/config/app_constants.dart
+++ mobile_app/lib/config/app_constants.dart
@@ -15,12 +15,12 @@ class AppInfo {
 // ── API ───────────────────────────────────────────────
 class ApiConstants {
   // Main backend API (existing)
-  //static const String baseUrl       = 'https://e-fine-sl-traffic-management-1.onrender.com/api';
-   static const String baseUrl    = 'http://10.231.145.6:5000/api'; // local dev
+  static const String baseUrl       = 'https://e-fine-sl-traffic-management-1.onrender.com/api';
+   //static const String baseUrl    = 'http://10.231.145.6:5000/api'; // local dev
 
   // Auth Microservice URL (update after Render deployment)
- // static const String authServiceUrl = 'https://e-fine-sl-auth-service.onrender.com';
-  static const String authServiceUrl = 'http://10.231.145.6:5000'; // local dev (pointing to main for now if merged)
+  static const String authServiceUrl = 'https://e-fine-sl-auth-service.onrender.com';
+  //static const String authServiceUrl = 'http://10.231.145.6:5000'; // local dev (pointing to main for now if merged)
 
   static const String walletBaseUrl = 'https://efine-mock-data-loader.onrender.com/api/wallet';
   static const int    connectTimeout = 30000;
